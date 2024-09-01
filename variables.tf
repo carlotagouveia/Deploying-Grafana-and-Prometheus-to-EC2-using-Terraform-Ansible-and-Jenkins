8ec9a961d351a6540ca4a382da3d0c81a8fce799
carlota <carlotagouveiaa@hotmail.com>
2024-09-01 18:34:00 +0000

    delete unused vars and add access_ip

--- variables.tf
+++ variables.tf
@@ -3,12 +3,8 @@ variable vpc_cidr {
     default = "10.123.0.0/16"
 }
 
-variable public_cidrs {
+variable access_ip {
     type = list(string)
-    default = ["10.123.1.0/24", "10.123.3.0/24"]
-}
-
-variable private_cidrs {
-    type = list(string)
-    default = ["10.123.2.0/24", "10.123.4.0/24"]
+    default = ["0.0.0.0/0"]
+    
 }
